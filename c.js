7a369e7c0e0e5ca0715960ab88d4032168895717
workonme <148727304+workonme@users.noreply.github.com>
2026-04-05 11:03:48 +0300

    Create c.js

---
+++ c.js
@@ -0,0 +1,28 @@
+window.productsGrid = 4;
+window.currency = '₽'
+window.monitoring = { id: "42403, 42415" };
+window.server_connect_1 = "46.174.48.216:28015";
+window.server_connect_2 = "46.174.48.216:1488";
+window.promo_copy_start = "RLMSTART";
+window.widget_discord_link = "taSEdyTyNg";
+
+const cssScript = document.createElement('script')
+cssScript.src = 'https://cdn.jsdelivr.net/gh/workonme/realmrust@main/newyork.js'
+document.head.appendChild(cssScript)
+
+const swiperCSS = document.createElement('link')
+swiperCSS.rel = 'stylesheet'
+swiperCSS.href = 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css'
+document.head.appendChild(swiperCSS)
+
+const swiperScript = document.createElement('script')
+swiperScript.src = 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js'
+swiperScript.onload = () => {
+  const script = document.createElement('script')
+  script.src = 'https://cdn.jsdelivr.net/gh/workonme/realmrust@main/denver.js'
+  script.async = true
+  script.onload = () => {
+  }
+  document.body.appendChild(script)
+}
+document.body.appendChild(swiperScript)

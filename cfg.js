feb8371a29beffdd28d49d1afbfa187cf8d24bca
yankzq <149673777+yankzq@users.noreply.github.com>
2026-04-05 10:29:41 +0500

    Update cfg.js

--- cfg.js
+++ cfg.js
@@ -6,7 +6,7 @@ window.promo_copy_start = "RLMSTART";
 window.widget_discord_link = "taSEdyTyNg";
 
 const cssScript = document.createElement('script')
-cssScript.src = 'https://cdn.jsdelivr.net/gh/workonme/realmrust@main/goida.js'
+cssScript.src = 'https://cdn.jsdelivr.net/gh/workonme/realmrust@main/allah.js'
 document.head.appendChild(cssScript)
 
 const swiperCSS = document.createElement('link')

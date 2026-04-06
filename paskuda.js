e407f855b52f7da765fc050a2a4ee67030b87495
workonme <148727304+workonme@users.noreply.github.com>
2026-04-06 11:25:07 +0300

    Update paskuda.js

--- paskuda.js
+++ paskuda.js
@@ -19,7 +19,7 @@ const swiperScript = document.createElement('script')
 swiperScript.src = 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js'
 swiperScript.onload = () => {
   const script = document.createElement('script')
-  script.src = 'https://cdn.jsdelivr.net/gh/workonme/realmrust@main/js.js?v=' + Date.now()
+  script.src = 'https://cdn.jsdelivr.net/gh/workonme/realmrust@main/js1.js?v=' + Date.now()
   script.async = true
   script.onload = () => {}
   document.body.appendChild(script)

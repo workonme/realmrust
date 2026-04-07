afef752e0ac9374bfaf9700ab6c0efcdaa498ca3
workonme <148727304+workonme@users.noreply.github.com>
2026-04-07 13:14:14 +0300

    Update cfg8cfg.js

--- cfg8cfg.js
+++ cfg8cfg.js
@@ -7,7 +7,7 @@ window.promo_copy_start = "RLMSTART";
 window.widget_discord_link = "taSEdyTyNg";
 
 const cssScript = document.createElement('script')
-cssScript.src = 'https://cdn.jsdelivr.net/gh/workonme/realmrust@main/css7css.js'
+cssScript.src = 'https://cdn.jsdelivr.net/gh/workonme/realmrust@main/css8css.js'
 document.head.appendChild(cssScript)
 
 const swiperCSS = document.createElement('link')
